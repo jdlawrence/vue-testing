91cb18e3c5ad9e59dabd713f2094e0850f62dfc5
Jamil Lawrence <jamil.lawrence@gmail.com>
2018-07-31 21:43:47 -0700

    Initial Commit

---
+++ hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <JamilBase></JamilBase>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import JamilBase from './components/JamilBase.vue';
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    JamilBase,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/JamilBase.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="JamilBase">
+    Welcome to my page!
+    <img
+      class="JamilBase__turtles"
+      src="./Teenage-mutant-ninja-turtles.jpg"
+    >
+    <img
+      class="JamilBase__mario"
+      src="./mario.svg"
+    >
+    <div class="JamilBase__dummy-div"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JamilBase',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.JamilBase {
+  color: blue;
+  font-size: 23px;
+
+  &__turtles {
+    width: 200px;
+  }
+
+  &__dummy-div {
+    background-color: red;
+    height: 150px;
+    width: 150px;
+    background: url(".")
+  }
+}
+</style>
+
